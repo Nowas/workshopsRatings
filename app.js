0c054f6084d4961528887a283d054a14a0b8f83c
Patryk Nowacki <nowas5@gmail.com>
2017-10-06 21:13:44 +0200

    mysql

--- app.js
+++ app.js
@@ -9,10 +9,5 @@ app.use(require('body-parser').json({ type: 'json' }))
 app.use(router)
 app.use(express.static(path.join(__dirname, './public')));
 
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./results.db');
-db.serialize(function() {
-    db.run("CREATE TABLE if not exists workshops_ratings (symbol TEXT, ratings INTEGER, ts TEXT)");
-    app.listen(process.env.PORT || 3000)
-})
+app.listen(process.env.PORT || 3000)
 
--- controllers/workshops.js
+++ controllers/workshops.js
@@ -6,13 +6,13 @@ var templateDetails = require('../templates/workshopSite')
 var pug = require('pug')
 var sqlite3 = require('sqlite3').verbose();
 
-
+var mysql = require('mysql');
 module.exports = function (config) {
 
     function getWorkshopList(request, response) {
         var workshopList = []
 
-        for(var propertyName in config.workshops) {
+        for (var propertyName in config.workshops) {
             workshopList.push({
                 symbol: propertyName,
                 name: config.workshops[propertyName]
@@ -25,39 +25,61 @@ module.exports = function (config) {
         response.send(html)
     }
 
-    function rateWorkshop(request, response){
+    function rateWorkshop(request, response) {
         var db = new sqlite3.Database('results.db');
-        db.run("INSERT into workshops_ratings (symbol, ratings, ts) VALUES ('"+
-            request.params.workshopSymbol+"',"+
-            request.params.workshopRatings+",'"+
-            new Date().toISOString()+
-        "')");
         
-        var html = pug.render(templateDetails, {
-            'workshopsSymbol': request.params.workshopSymbol,
-            'workshopsName': config.workshops[request.params.workshopSymbol],
-            'info':'Thx'
-        })
-        response.send(html)
-    }
-
-    function  getWorkshopDetails(request, response){
-        var html = pug.render(templateDetails, {
-            'workshopsSymbol': request.params.workshopSymbol,
-            'workshopsName': config.workshops[request.params.workshopSymbol],
-            'info':'Thx'
-        })
-        response.send(html)
-    }
-
-    function  getWorkshopResults(request, response){
-        var db = new sqlite3.Database('results.db');
-        db.all("SELECT * FROM workshops_ratings",function(err,rows){
-            response.send(rows)
+        var connection = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USR,
+            password: process.env.DB_PWD,
+            database: process.env.DB_NAME,
+            port: 3306
         });
 
+        var ratings = {
+            symbol: request.params.workshopSymbol,
+            ratings: request.params.workshopRatings,
+            ts: new Date().toISOString()
+        }
+        connection.connect();
+        connection.query('INSERT INTO workshops_ratings SET ?', ratings, function (error, results, fields) {
+            connection.end();
+        });
+
+        var html = pug.render(templateDetails, {
+            'workshopsSymbol': request.params.workshopSymbol,
+            'workshopsName': config.workshops[request.params.workshopSymbol],
+            'info': 'Thx'
+        })
+        response.send(html)
     }
-    
+
+    function getWorkshopDetails(request, response) {
+        var html = pug.render(templateDetails, {
+            'workshopsSymbol': request.params.workshopSymbol,
+            'workshopsName': config.workshops[request.params.workshopSymbol],
+            'info': 'Thx'
+        })
+        response.send(html)
+    }
+
+    function getWorkshopResults(request, response) {
+        var connection = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USR,
+            password: process.env.DB_PWD,
+            database: process.env.DB_NAME,
+            port: 3306
+        });
+
+        connection.connect();
+        connection.query('SELECT * FROM workshops_ratings', function (error, results, fields) {
+            
+            response.send(results)
+            connection.end();
+        });
+    }
+
     return {
         getWorkshopList,
         getWorkshopDetails,
